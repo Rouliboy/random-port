76e5687f59edac407f55286dddb58ac299b750dd
Julien Vienne <you@example.com>
2020-10-20 23:05:01 +0200

    COmmit 2

--- src/main/java/com/jvi/random/RandomPortApplication.java
+++ src/main/java/com/jvi/random/RandomPortApplication.java
@@ -9,15 +9,14 @@ public class RandomPortApplication {
 		ApplicationContext ctx = new
 				ClassPathXmlApplicationContext( "application-context.xml" );
 
-		RandomPortGenerator rpg1 = ctx.getBean(RandomPortGenerator.class);
-		System.out.println(rpg1.generatePortInRange(1,2));
-		System.out.println(rpg1.generatePortInRange("3-5"));
-
 		ServiceEngine serviceEngine = (ServiceEngine) ctx.getBean("serviceEngine");
 		System.out.println(serviceEngine.getName());
-		System.out.println(serviceEngine.getServicePort());
 
+		ServiceEngine serviceEngine2 = (ServiceEngine) ctx.getBean("serviceEngine2");
+		System.out.println(serviceEngine2.getServicePort());
 
+		ServiceEngine serviceEngine3 = (ServiceEngine) ctx.getBean("serviceEngine3");
+		System.out.println(serviceEngine3.getServicePort());
 	}
 
 }
